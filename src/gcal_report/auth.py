94f7756232fe83d2436b53a38aad591e203b0ac3
Chase Seibert <chase.seibert+github@gmail.com>
2014-12-29 17:00:07 -0800

    API query works

--- src/gcal_report/auth.py
+++ src/gcal_report/auth.py
@@ -1,8 +1,12 @@
-import tempfile
+import os
 
+import httplib2
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.tools import run
 from oauth2client.file import Storage
+
+
+_CREDENTIALS = os.path.expanduser('~/.gcal-report-creds')
 
 
 def generate_access_token(client_id, client_secret):
@@ -11,7 +15,16 @@ def generate_access_token(client_id, client_secret):
         client_secret=client_secret,
         scope='https://www.googleapis.com/auth/calendar.readonly',
         redirect_uri='http://example.com/auth_return')
-    tmp = tempfile.NamedTemporaryFile()
-    storage = Storage(tmp.name)
+    storage = Storage(_CREDENTIALS)
     credentials = run(flow, storage)
     return credentials.access_token
+
+
+def get_authorized_http():
+    storage = Storage(_CREDENTIALS)
+    credentials = storage.get()
+    if credentials is None or credentials.invalid:
+        print 'Invalid credentials, you need to login again'
+        exit(1)
+    http = httplib2.Http()
+    return credentials.authorize(http)
--- src/gcal_report/gcal.py
+++ src/gcal_report/gcal.py
@@ -1,11 +1,18 @@
+from apiclient.discovery import build
 import httplib2
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.tools import run
 from oauth2client.file import Storage
 
+from gcal_report import auth
 from gcal_report import settings
 
 
+def _build_service():
+    http = auth.get_authorized_http()
+    return build('calendar', 'v3', http=http)
+
+
 def get_calendar_list():
-    creds = _get_access_token()
-    print creds
+    service = _build_service()
+    return service.calendarList().list().execute()
